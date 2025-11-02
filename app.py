faa141538b87a89a609b2fc558c1ed7ad4a97966
Oladele  Paul <98545430+king-p1@users.noreply.github.com>
2025-11-01 20:49:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,63 @@
 
+# A
+a = 1
+b = 2
+c = 5
+while a < c:
+    a = a + 1
+    b = b + c
+print(a, b, c)
+
+# B
+d = 4
+e = 6
+f = 7
+while f > d:
+    d = d + 1
+    e = e - 1
+print(d, e, f)
+
+
+# C
+g = 4
+h = 6
+while g < h:
+    g = g + 1
+print(g, h)
+
+
+# D
+j = 2
+k = 5
+n = 9
+while j < k:
+    m = 6
+    while m < n:
+        print("Goodbye")
+        m = m + 1
+    j = j + 1
+
+
+# E
+j = 2
+k = 5
+m = 6
+n = 9
+while j < k:
+    while m < n:
+        print("Hello")
+        m = m + 1
+    j = j + 1
+
+
+
+# F
+p = 2
+q = 4
+while p < q:
+    print("Adios")
+    r = 1
+    while r < q:
+        print("Adios")
+        r = r + 1
+    p = p + 1
